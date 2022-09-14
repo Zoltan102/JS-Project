085ae097be20f0aef3af2f736d90c74a0c7e6691
zoli <suvegeszoltan28@gmail.com>
2022-09-14 12:47:03 +0200

    webpack

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+import _ from 'lodash';
 import { random } from './maths.js';
 import { NAME, LOREM } from './texts/special.js'
 
 console.log("Hello");
 console.log(random(5, 10));
 console.log(NAME + ' ' + LOREM);
+let a1 = [ 1, 455, 6 ];
+let a2 = [ 454, 33, 1 ];
+let a3 = [ 2 ];
+
+let a4 = _.concat(a1, a2, a3);
+console.log(a4);
