fdf20c0b5bcd592742227de92c71168dae65511d
zoli <suvegeszoltan28@gmail.com>
2022-09-14 12:30:38 +0200

    lodash

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { random } from './maths.js'
+import { random } from './maths.js';
+import { NAME, LOREM } from './texts/special.js'
 
 console.log("Hello");
-console.log(random(5, 10))
+console.log(random(5, 10));
+console.log(NAME + ' ' + LOREM);
---
+++ texts/special.js
@@ -0,0 +1,2 @@
+export const LOREM = "Lorem Ipsum";
+export const NAME = "Minta Diák";
